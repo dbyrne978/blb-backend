4361a3d333a11995cb7d50d125bbaac0c08fa43a
Daniel Byrne <dbyrne978@gmail.com>
2023-11-28 17:45:38 -0700

    Extract router into separate module

---
+++ controllers/backlogItems.js
@@ -0,0 +1,67 @@
+const backlogItemsRouter = require('express').Router()
+const BacklogItem = require('../models/backlogItem')
+
+backlogItemsRouter.get('/:id', (request, response, next) => {
+  BacklogItem
+    .findById(request.params.id)
+    .then(backlogItem => {
+      if (backlogItem) {
+        response.json(backlogItem)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+backlogItemsRouter.delete('/:id', (request, response, next) => {
+  BacklogItem
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+backlogItemsRouter.get('/', (request, response, next) => {
+  BacklogItem
+    .find({})
+    .then(backlogItems => {
+      response.json(backlogItems)
+    })
+    .catch(error => next(error))
+})
+
+backlogItemsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const backlogItem = new BacklogItem({
+    title: body.title,
+    format: body.format,
+    completionStatus: body.completionStatus || "Backlog"
+  })
+
+  backlogItem
+    .save()
+    .then(savedBacklogItem => response.json(savedBacklogItem))
+    .catch(error => next(error))
+})
+
+backlogItemsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const updatedBacklogItem = {
+    title: body.title,
+    format: body.format,
+    completionStatus: body.completionStatus
+  }
+
+  BacklogItem
+    .findByIdAndUpdate(request.params.id, updatedBacklogItem, { new: true })
+    .then(updatedBacklogItem => {
+      response.json(updatedBacklogItem)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = backlogItemsRouter
