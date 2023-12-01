a3abdde2860f3ae750452b035f478cdb63f55776
Daniel Byrne <dbyrne978@gmail.com>
2023-11-30 17:12:16 -0700

    Initialize backlogItems during testing

--- tests/backlogItemApi.test.js
+++ tests/backlogItemApi.test.js
@@ -1,14 +1,43 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const BacklogItem = require('../models/backlogItem')
 
-test('backlogItems are returned as json', async () => {
-  await api
-    .get('/api/backlogItems')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+const initialBacklogItems = [
+  {
+    title: 'Final Fantasy XII',
+    format: 'Game',
+    completionStatus: 'Complete',
+  },
+  {
+    title: 'The Witcher',
+    format: 'TV Show',
+    completionStatus: 'Backlog',
+  },
+]
+
+beforeEach(async () => {
+  await BacklogItem.deleteMany({})
+  let backlogItemObject = new BacklogItem(initialBacklogItems[0])
+  await backlogItemObject.save()
+  backlogItemObject = new BacklogItem(initialBacklogItems[1])
+  await backlogItemObject.save()
+})
+
+test('all backlogItems are returned', async () => {
+  const response = await api.get('/api/backlogItems')
+
+  expect(response.body).toHaveLength(initialBacklogItems.length)
+})
+
+test('a specific backlogItem is within the returned backlogItems', async () => {
+  const response = await api.get('/api/backlogItems')
+
+  const contents = response.body.map(r => r.title)
+  expect(contents).toContain(
+    'Final Fantasy XII'
+  )
 })
 
 afterAll(async () => {
