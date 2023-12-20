b8cbdf8673bb70c65006b010c27fb5ef9a74cbdf
Daniel Byrne <dbyrne978@gmail.com>
2023-12-20 12:12:14 -0700

    Group tests under descriptions

--- tests/backlogItemApi.test.js
+++ tests/backlogItemApi.test.js
@@ -12,104 +12,116 @@ beforeEach(async () => {
   await BacklogItem.insertMany(helper.initialBacklogItems)
 })
 
-test('a specific backlogItem can be viewed', async () => {
-  const backlogItemsAtStart = await helper.backlogItemsInDb()
+describe('when there is initially some backlogItems saved', () => {
+  test('all backlogItems are returned', async () => {
+    const response = await api.get('/api/backlogItems')
+  
+    expect(response.body).toHaveLength(helper.initialBacklogItems.length)
+  })
 
-  const backlogItemToView = backlogItemsAtStart[0]
-
-  const resultingBacklogItem = await api
-    .get(`/api/backlogItems/${backlogItemToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  expect(resultingBacklogItem.body).toEqual(backlogItemToView)
+  test('a specific backlogItem is within the returned backlogItems', async () => {
+    const response = await api.get('/api/backlogItems')
+  
+    const contents = response.body.map(r => r.title)
+    expect(contents).toContain(
+      'Final Fantasy XII'
+    )
+  })
 })
 
-test('all backlogItems are returned', async () => {
-  const response = await api.get('/api/backlogItems')
-
-  expect(response.body).toHaveLength(helper.initialBacklogItems.length)
+describe('viewing a specific backlogItem', () => {
+  test('succeeds with valid id', async () => {
+    const backlogItemsAtStart = await helper.backlogItemsInDb()
+  
+    const backlogItemToView = backlogItemsAtStart[0]
+  
+    const resultingBacklogItem = await api
+      .get(`/api/backlogItems/${backlogItemToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    expect(resultingBacklogItem.body).toEqual(backlogItemToView)
+  })
 })
 
-test('a specific backlogItem is within the returned backlogItems', async () => {
-  const response = await api.get('/api/backlogItems')
+describe('addition of a new backlogItem', () => {
+  test('succeeds with valid data', async () => {
+    const newBacklogItem = {
+      title: 'There Will Be Blood',
+      format: 'Movie',
+      completionStatus: 'Backlog',
+    }
+  
+    await api
+      .post('/api/backlogItems')
+      .send(newBacklogItem)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const backlogItemsAtEnd = await helper.backlogItemsInDb()
+    expect(backlogItemsAtEnd).toHaveLength(helper.initialBacklogItems.length + 1)
+  
+    const contents = backlogItemsAtEnd.map(item => item.title)
+    expect(contents).toContain('There Will Be Blood')
+  })
+  
+  test('fails with status code 400 if it has no title', async () => {
+    const newBacklogItem = {
+      format: 'Movie',
+      completionStatus: 'Backlog',
+    }
 
-  const contents = response.body.map(r => r.title)
-  expect(contents).toContain(
-    'Final Fantasy XII'
-  )
+    await api
+      .post('/api/backlogItems')
+      .send(newBacklogItem)
+      .expect(400)
+
+    const backlogItemsAtEnd = await helper.backlogItemsInDb()
+
+    expect(backlogItemsAtEnd).toHaveLength(helper.initialBacklogItems.length)
+  })
+
 })
 
-test('a valid backlogItem can be added', async () => {
-  const newBacklogItem = {
-    title: 'There Will Be Blood',
-    format: 'Movie',
-    completionStatus: 'Backlog',
-  }
+describe('deletion of a backlogItem', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const backlogItemsAtStart = await helper.backlogItemsInDb()
+    const backlogItemToDelete = backlogItemsAtStart[0]
+  
+    await api
+      .delete(`/api/backlogItems/${backlogItemToDelete.id}`)
+      .expect(204)
+  
+    const backlogItemsAtEnd = await helper.backlogItemsInDb()
+  
+    expect(backlogItemsAtEnd).toHaveLength(
+      helper.initialBacklogItems.length - 1
+    )
+  
+    const contents = backlogItemsAtEnd.map(r => r.title)
+  
+    expect(contents).not.toContain(backlogItemToDelete.title)
+  })
 
-  await api
-    .post('/api/backlogItems')
-    .send(newBacklogItem)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const backlogItemsAtEnd = await helper.backlogItemsInDb()
-  expect(backlogItemsAtEnd).toHaveLength(helper.initialBacklogItems.length + 1)
-
-  const contents = backlogItemsAtEnd.map(item => item.title)
-  expect(contents).toContain('There Will Be Blood')
 })
 
-test('backlogItem without title is not added', async () => {
-  const newBacklogItem = {
-    format: 'Movie',
-    completionStatus: 'Backlog',
-  }
-
-  await api
-    .post('/api/backlogItems')
-    .send(newBacklogItem)
-    .expect(400)
-
-  const backlogItemsAtEnd = await helper.backlogItemsInDb()
-
-  expect(backlogItemsAtEnd).toHaveLength(helper.initialBacklogItems.length)
-})
-
-test('a backlogItem can be deleted', async () => {
-  const backlogItemsAtStart = await helper.backlogItemsInDb()
-  const backlogItemToDelete = backlogItemsAtStart[0]
-
-  await api
-    .delete(`/api/backlogItems/${backlogItemToDelete.id}`)
-    .expect(204)
-
-  const backlogItemsAtEnd = await helper.backlogItemsInDb()
-
-  expect(backlogItemsAtEnd).toHaveLength(
-    helper.initialBacklogItems.length - 1
-  )
-
-  const contents = backlogItemsAtEnd.map(r => r.title)
-
-  expect(contents).not.toContain(backlogItemToDelete.title)
-})
-
-test('a backlogItem can be edited', async () => {
-  const backlogItemsAtStart = await helper.backlogItemsInDb()
-  const backlogItemToEdit = backlogItemsAtStart[0]
-
-  await api
-    .put(`/api/backlogItems/${backlogItemToEdit.id}`)
-    .send({completionStatus: 'Playing'})
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const backlogItemsAtEnd = await helper.backlogItemsInDb()
-  expect(backlogItemsAtEnd).toHaveLength(helper.initialBacklogItems.length)
-
-  const editedBacklogItem = backlogItemsAtEnd[0]
-  expect(editedBacklogItem.completionStatus).toEqual('Playing')
+describe('editing of a backlogItem', () => {
+  test('succeeds with valid data', async () => {
+    const backlogItemsAtStart = await helper.backlogItemsInDb()
+    const backlogItemToEdit = backlogItemsAtStart[0]
+  
+    await api
+      .put(`/api/backlogItems/${backlogItemToEdit.id}`)
+      .send({completionStatus: 'Playing'})
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const backlogItemsAtEnd = await helper.backlogItemsInDb()
+    expect(backlogItemsAtEnd).toHaveLength(helper.initialBacklogItems.length)
+  
+    const editedBacklogItem = backlogItemsAtEnd[0]
+    expect(editedBacklogItem.completionStatus).toEqual('Playing')
+  })
 })
 
 afterAll(async () => {
